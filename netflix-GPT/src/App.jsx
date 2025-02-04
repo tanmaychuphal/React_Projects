5b5a5bfe47ab4a3890c552331b7238b0da985c2c
tanmaychuphal <tchuphal2001@gmail.com>
2025-02-04 12:30:54 +0530

    first commit

---
+++ netflix-GPT/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-primary">NAMASTE TANMAY</h1>
+    </>
+  )
+}
+
+export default App
